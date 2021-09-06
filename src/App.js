8bea26b7de8f60d5b0bb56102b2e761a26fd38f7
sevangmb <sevans@hotmail.fr>
2021-09-06 21:00:10 +0200

    cleaning package.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
+import Competences from './components/Competences'
+import Plex from './components/Plex'
+import Exemple2 from './components/Exemple2'
+import data from "./data"; 
+import CenteredTabs from './components/menu';
+import Home from './components/home';
+import Cv from './components/cv';
 
-function App() {
+const App = () => {
+  const [currentTab, setCurrentTab] = useState(0); 
+
+  const onChangeTab = (event, newValue) => {
+      setCurrentTab(newValue);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <CenteredTabs current={currentTab} onChange={onChangeTab} />
 
-export default App;
+      {currentTab === 1 && (<Competences />)}
+      {currentTab === 2 && (<Exemple2 title="Expériences" experiences={data.Experiences} />)}
+      {currentTab === 3 && (<Cv />)}
+      {currentTab === 4 && (<Plex />)}
+      {currentTab === 0 && (<Home />)}
+      
+    </ThemeProvider>
+  )
+};
+
+export default App
