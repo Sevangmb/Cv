6321528a72e791ee0601373d23021fbc1e08c353
sevangmb <sevans@hotmail.fr>
2021-09-07 18:55:30 +0200

    bug predoflix et competances

--- src/components/Exemple3.js
+++ src/components/Exemple3.js
@@ -1,32 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import data from "../data"; 
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-class Exemple3 extends Component {
-	render() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginTop: '50px',
+    },
+    paper: {
+      padding: theme.spacing(3),
+      textAlign: 'center',
+      marginTop: '20px',
+      color: theme.palette.text.secondary,
+    },
+
+}));
+
+const Exemple3 = () => {
+
+  const classes = useStyles();
+
 		return (
-            <div>
+            <div className={classes.root}>
                 {
                   data.Skills.map((skill) => {
                     return (
-                      <div>
-                        <h4>{skill.Area}</h4>
+                      <Grid container justifyContent="center" item xs={12}>
+                        <Paper elevation={3} className={classes.paper}>
+                        <Typography variant="h3">{skill.Area}</Typography>
                         <ul>
                           {
                             skill.SkillSet.map((skillDetail) => {
                               return (
-                                  <li>
-                                    {skillDetail.Name}
-                                  </li>
+                                  
+                                    <Typography variant="body1">{skillDetail.Name}</Typography>
+                                  
                               );
                             })
                           }
                         </ul>
-                      </div>
+                        </Paper>
+                        </Grid>
                     );
                   })
                 } 
             </div>
         );
     }
-} 
-export default Exemple3;
+
+
+export default Exemple3
